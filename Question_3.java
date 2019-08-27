47a645eee299a6117f0b3134049a7f2d657eb813
anirudha674 <53290807+anirudha674@users.noreply.github.com>
2019-08-27 21:12:06 +0530

    Add files via upload

---
+++ Question_3.java
@@ -0,0 +1,16 @@
+
+public class Question_3 {
+
+	public static void main(String[] args) {
+		int a=7,b=9;
+		System.out.println(a&b);
+		System.out.println(a|b);
+		System.out.println(a^b);
+		System.out.println(a<<b);
+		System.out.println(a>>b);
+		
+		
+
+	}
+
+}
---
+++ Question_4.java
@@ -0,0 +1,21 @@
+
+public class Question_4 {
+	public static void main(String[] args) {
+		int a=10,b=20,c=30,d=40,greatest=0;
+		greatest=a>b?a:b;
+		greatest=c>greatest?c:greatest;
+		greatest=d>greatest?d:greatest;
+		System.out.println(greatest);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
